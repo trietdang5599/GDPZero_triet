28648c841c51d81a28cf18ace53e0b6fa6269f4d
trietdang5599 <trietdang05051999@gmail.com>
2025-10-08 22:28:36 +0800

    add option deepseek

--- runners/gdpzero.py
+++ runners/gdpzero.py
@@ -59,11 +59,13 @@ def main(cmd_args):
 		SysModel = PersuaderChatModel
 		UsrModel = PersuadeeChatModel
 		SysPlanner = P4GChatSystemPlanner
-	elif cmd_args.llm in ['qwen2.5-7b', 'llamda-3-8b']:
+	elif cmd_args.llm in ['qwen2.5-7b', 'llamda-3-8b', 'deepseek-r1']:
 		if cmd_args.llm == 'qwen2.5-7b':
 			model_source = 'Qwen/Qwen2.5-7B-Instruct'
 		elif cmd_args.llm == 'llamda-3-8b':
 			model_source = 'meta-llama/Meta-Llama-3-8B-Instruct'
+		elif cmd_args.llm == 'deepseek-r1':
+			model_source = 'deepseek-ai/DeepSeek-R1'
 		backbone_model = LocalModel(model_source, trust_remote_code=True)
 		SysModel = PersuaderChatModel
 		UsrModel = PersuadeeChatModel
@@ -260,7 +262,7 @@ def main(cmd_args):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--output', type=str, default="", help='output file (autofills if empty)')
-	parser.add_argument('--llm', type=str, default="code-davinci-002", choices=["code-davinci-002", "chatgpt", "gpt-3.5-turbo", "gpt2", "qwen2.5-7b", "llamda-3-8b", "local"], help='Backbone model identifier')
+	parser.add_argument('--llm', type=str, default="code-davinci-002", choices=["code-davinci-002", "chatgpt", "gpt-3.5-turbo", "gpt2", "qwen2.5-7b", "llamda-3-8b", "deepseek-r1", "local"], help='Backbone model identifier')
 	parser.add_argument('--gen_sentences', type=int, default=-1, help='number of sentences to generate from the llm. Longer ones will be truncated by nltk.')
 	parser.add_argument('--num_mcts_sims', type=int, default=20, help='number of mcts simulations')
 	parser.add_argument('--max_realizations', type=int, default=3, help='number of realizations per mcts state')
