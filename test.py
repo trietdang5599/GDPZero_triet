3a2c7dd1aba16f1c7bfdebc9738c7479ec370e69
trietdang5599 <trietdang05051999@gmail.com>
2025-10-09 11:04:24 +0800

    add option llamda for test

--- test.py
+++ test.py
@@ -19,7 +19,8 @@ def main(args):
 	# backbone_model = OpenAIModel('text-davinci-003')
 	if args.judge in ['gpt-3.5-turbo', 'gpt-5-nano', 'gpt-4o-mini']:
 		backbone_model = OpenAIChatModel(args.judge)
-	elif args.judge in ['gpt2', 'Qwen/Qwen2.5-7B-Instruct', 'Qwen/Qwen2.5-0.5B-Instruct']:
+	elif args.judge in ['gpt2', 'Qwen/Qwen2.5-7B-Instruct', 'Qwen/Qwen2.5-0.5B-Instruct',
+                     'meta-llama/Meta-Llama-3-8B-Instruct']:
 		backbone_model = LocalModel(args.judge)
 	elif args.judge == 'chatgpt':
 		backbone_model = AzureOpenAIChatModel(args.judge)
@@ -82,7 +83,8 @@ def main(args):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', type=str, help='path to the data file for comparing against human in p4g. See P4GEvaluator documentation to see the format of the file.')
-	parser.add_argument('--judge', type=str, default='gpt-3.5-turbo', help='which judge to use.', choices=['gpt-3.5-turbo', 'gpt-5-nano', 'gpt-4o-mini', 'chatgpt', 'gpt2', 'Qwen/Qwen2.5-7B-Instruct', 'Qwen/Qwen2.5-0.5B-Instruct'])
+	parser.add_argument('--judge', type=str, default='gpt-3.5-turbo', help='which judge to use.', choices=['gpt-3.5-turbo', 'gpt-5-nano', 'gpt-4o-mini', 'chatgpt', 'gpt2',
+                                                                                                        'Qwen/Qwen2.5-7B-Instruct', 'Qwen/Qwen2.5-0.5B-Instruct', 'meta-llama/Meta-Llama-3-8B-Instruct'])
 	parser.add_argument('--h2h', type=str, default='', help='path to the data file for head to head comparison. If empty compare against human in p4g.')
 	parser.add_argument("--output", type=str, default='', help="output file")
 	parser.add_argument("--debug", action='store_true', help="debug mode")
