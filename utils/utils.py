8d9e4f014e7bb5d9cb4df903b631b087cd3ec8fd
trietdang5599 <trietdang05051999@gmail.com>
2025-10-14 14:24:33 +0800

    update

--- utils/utils.py
+++ utils/utils.py
@@ -18,7 +18,10 @@ def set_determinitic_seed(seed):
 	np.random.seed(seed)
 	torch.manual_seed(seed)
 	torch.cuda.manual_seed_all(seed)
-	torch.use_deterministic_algorithms(True)
+	try:
+		torch.use_deterministic_algorithms(True, warn_only=True)
+	except TypeError:
+		torch.use_deterministic_algorithms(True)
 	torch.backends.cudnn.deterministic = True
 	torch.backends.cudnn.benchmark = False
 	return
